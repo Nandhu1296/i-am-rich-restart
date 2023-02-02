138bb9dffbf03e68a63159b1a65ab4e5135a9aac
Access-Lab <wsuaccesslab@gmail.com>
2023-02-02 12:07:48 -0600

    Initial Commit

---
+++ i am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by wh-308 on 2/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
